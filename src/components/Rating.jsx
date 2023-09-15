dc22f851ab76f5cddead07feb98db3309a5f344c
Naveen <naveen1808@live.in>
2023-09-15 10:47:48 +0530

    ratings component and styles without buttons form

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,18 @@
+import styles from "./Rating.module.css";
+
 function Rating() {
-  return <div>rating component</div>;
+  return (
+    <div>
+      <div className={styles.starimg_container}>
+        <img src="/icon-star.svg" alt="star icon" aria-hidden={true} />
+      </div>
+      <h1>How did we do?</h1>
+      <p>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+    </div>
+  );
 }
 
 export default Rating;
